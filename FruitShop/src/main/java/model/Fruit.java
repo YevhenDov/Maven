e9151f4c77adf35cf9685e2bdd82865962ee8195
YevhenDov <dovgenko.en@gmail.com>
2019-04-01 00:00:33 +0300

    Start

---
+++ FruitShop/src/main/java/model/Fruit.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.Date;
+
+public class Fruit {
+    private FruitType type;
+    private int shelfLife;
+    private Date date;
+    private double price;
+
+
+    public Fruit() {
+    }
+
+    public Fruit(FruitType type, int shelfLife, Date date, double price) {
+        this.type = type;
+        this.shelfLife = shelfLife;
+        this.date = date;
+        this.price = price;
+    }
+
+    public FruitType getType() {
+        return type;
+    }
+
+    public int getShelfLife() {
+        return shelfLife;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this){
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Fruit fruit = (Fruit) obj;
+        return (type == fruit.type)
+                && (shelfLife == fruit.shelfLife)
+                && (price == fruit.price)
+                && (date.equals(this.date));
+    }
+}
---
+++ FruitShop/src/main/java/service/FruitShop.java
@@ -0,0 +1,84 @@
+package service;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import model.Fruit;
+import model.FruitType;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FruitShop {
+    public List<Fruit> fruits = new ArrayList<>();
+    Gson gson = new Gson();
+
+    public void addFruits(String pathToJsonFile) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(pathToJsonFile));
+            Type fruitsList = new TypeToken<ArrayList<Fruit>>() {}.getType();
+            fruits = gson.fromJson(bufferedReader, fruitsList);
+        } catch (IOException e) {
+        }
+        System.out.println(fruits);
+    }
+
+    public void save(String pathToJsonFile) {
+        try {
+            Writer writer = new FileWriter(pathToJsonFile);
+            gson.toJson(fruits, writer);
+        } catch (IOException e) {
+        }
+    }
+
+    public void load(String pathToJsonFile) {
+        fruits.clear();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(pathToJsonFile));
+            Type fruitsList = new TypeToken<ArrayList<Fruit>>() {
+            }.getType();
+            fruits = gson.fromJson(bufferedReader, fruitsList);
+        } catch (IOException e) {
+        }
+        System.out.println(fruits);
+    }
+
+    public List<Fruit> getSpoiledFruits(Date date) {
+        return fruits.stream().filter(fruit -> getSpoiledDate(fruit).before(date)).collect(Collectors.toList());
+    }
+
+    public List<Fruit> getSpoiledFruits(Date date, FruitType type) {
+        return getSpoiledFruits(date).stream().filter(fruit -> fruit.getType() == type).collect(Collectors.toList());
+    }
+
+    public List<Fruit> getAddedFruits(Date date) {
+        return fruits.stream().filter(fruit -> fruit.getDate().equals(date)).collect(Collectors.toList());
+    }
+
+    public List<Fruit> getAddedFruits(Date date, FruitType type) {
+        return getSpoiledFruits(date).stream().filter(fruit -> fruit.getType() == type).collect(Collectors.toList());
+    }
+
+    public List<Fruit> getAvailableFruits(Date date) {
+        return fruits.stream().filter(fruit -> getSpoiledDate(fruit).after(date)).collect(Collectors.toList());
+    }
+
+    public List<Fruit> getAvailableFruits(Date date, FruitType type) {
+        return getSpoiledFruits(date).stream().filter(e -> e.getType() == type).collect(Collectors.toList());
+    }
+
+    public Date getSpoiledDate(Fruit fruit) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fruit.getDate());
+        calendar.add(Calendar.DAY_OF_MONTH, fruit.getShelfLife());
+        return calendar.getTime();
+    }
+}
