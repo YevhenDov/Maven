0e91d419604fabeceb0a0e6cd3dffde62b2b78ae
YevhenDov <dovgenko.en@gmail.com>
2019-04-12 17:55:53 +0300

    Added adapter

---
+++ Patterns/src/main/java/com/adapter/CarAdapterFromVolvo.java
@@ -0,0 +1,7 @@
+package com.adapter;
+
+public class CarAdapterFromVolvo extends Volvo implements Car{
+    public void move() {
+        volvoMove();
+    }
+}
---
+++ Patterns/src/main/java/com/adapter/Volvo.java
@@ -0,0 +1,7 @@
+package com.adapter;
+
+public class Volvo {
+    public void volvoMove(){
+        System.out.println("Volvo move...");
+    }
+}
